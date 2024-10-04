909c631dfd952a2caa0fdb69e5831f9cbc907a0a
Aldo Perri <130299266+perri99@users.noreply.github.com>
2024-10-04 16:59:07 +0200

    sistema vite

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 from tommaso import Tommaso
 from final_boss import PaoloCasciato
 
-#primo livello
-Tom = Tommaso()
-while Tom.get_punteggio() < 2:
-    Tom.prima_domanda()
-    Tom.seconda_domanda()
-    Tom.terza_domanda()
-    Tom.vittoria()
+vite = 3
 
-#livello finale
-        
-paolo=PaoloCasciato()
-vittoria= paolo.vittoria()
-if vittoria == True:
-    print('Hai vinto il gioco')
+while vite > 0:
+    #primo livello
+    Tom = Tommaso()
+    while Tom.get_punteggio() < 2:
+        Tom.prima_domanda()
+        Tom.seconda_domanda()
+        Tom.terza_domanda()
+        if Tom.vittoria() == False: 
+            vite -=1
+            if vite == 0: break  
+
+    #livello finale
+    if vite == 0: break        
+    paolo = PaoloCasciato()
+    vittoria = paolo.vittoria()
+    if vittoria == True:
+        #print('Hai vinto il gioco')
+        vite = -1
+    else:
+        print('Sei caduto miseramente, hai perso una vita')
+        vite -= 1
+
+if vite == -1:
+    print('Gioco finito, hai vinto!!')
 else:
-    print('Sei caduto miseramente, game over')
+    print('Game Over, hai perso tutto')
